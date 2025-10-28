0496cbc189ee721995a584db442f80b1c222d811
Muhammad Hilman <mhilmansabli@gmail.com>
2025-10-28 18:56:58 +0800

    added react files

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+
+function ListGroup() {
+  const items = [
+    'New York',
+    'San Francisco',
+    'Tokyo',
+    'London',
+    'Paris'
+  ];
+  
+  return (
+  <>
+    <h1>List</h1>
+    <ul className="list-group">
+        {items.map(item => <li>{item}</li>)}
+    </ul>
+  </>
+  );
+}
+
+export default ListGroup;
